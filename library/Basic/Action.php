cad7b162e1c4f7d2cc3507e12583a8973f83f69e
Sjon Hortensius <sjon@hortensius.net>
2010-01-10 12:43:31 +0000

    config_example - added Basic 	- added Database to factory 	- moved 'Start the action' block to _dispatch 	- autoLoad() now ignores any errors Action 	- cleanup 	- end() fixed logs Controller 	- added _initDatabase() 	- added handleLastModified() Database - added Exception - autoCreate() classname > className Log 	- added getStatistics() 	- added getLogs() 	- added getTimers() Model - added Template - cleanup Userinput - added _clean()

--- library/Basic/Action.php
+++ library/Basic/Action.php
@@ -6,16 +6,18 @@ class Basic_Action
 	public $encoding = 'ISO-8859-15';
 	public $base_href;
 
+	public $templatesShown = array();
+	public $templateName;
+
+/*
 	var $userinput;
 	var $userinput_config;
 	var $userinput_validates;
+*/
+	public $lastModified;
+	public $cacheLength = 0;
 
-	var $last_modified;
-	var $cache_length;
-
-	var $action_is_valid;
-	var $templatesShown = array();
-	var $template_name;
+//	var $action_is_valid;
 
 	public function __construct()
 	{
@@ -23,21 +25,21 @@ class Basic_Action
 //		$this->userinput_validates =& $this->engine->userinput->required_validates;
 
 		$protocol = (isset($_SERVER['HTTPS']) && 'on' == $_SERVER['HTTPS']) ? 'https' : 'http';
-		$this->base_href = $protocol .'://' . $_SERVER['SERVER_NAME'] . Basic::$config->site['url_base'] .'/';
+		$this->base_href = $protocol .'://' . $_SERVER['SERVER_NAME'] . Basic::$config->site['baseUrl'] .'/';
 	}
 
 	function init()
 	{
-		if (!isset($this->template_name))
-			$this->template_name = $this->engine->action;
+		if (!isset($this->templateName))
+			$this->templateName = Basic::$controller->action;
 
-		$this->engine->handle_last_modified();
+		Basic::$controller->handleLastModified();
 
 		if (!headers_sent())
 			header('Content-Type: '.$this->contentType .'; charset='. $this->encoding);
 
 		try {
-			$this->show_template('header');
+			$this->showTemplate('header');
 		} catch (TemplateException $e){}
 	}
 
@@ -45,32 +47,32 @@ class Basic_Action
 	{
 		try
 		{
-			$this->show_template($this->template_name);
+			$this->showTemplate($this->template_name);
 		} catch (TemplateException $e) {}
 	}
 
 	function end()
 	{
-		$this->timers = $this->engine->log->short_output();
+		$this->timers = Basic::$log->getStatistics();
 
 		try {
-			$this->show_template('footer');
+			$this->showTemplate('footer');
 		} catch (TemplateException $e){}
 
 		if (isset($_GET['debug']))
 		{
 			$data = $GLOBALS;
 			unset($data['GLOBALS'], $data['HTTP_POST_VARS'], $data['HTTP_GET_VARS'], $data['HTTP_SERVER_VARS'], $data['HTTP_COOKIE_VARS'], $data['HTTP_ENV_VARS'], $data['HTTP_POST_FILES']);
-			engine::pr($data);
+			print_r($data);
 		}
 
-		if ($this->content_type == 'text/html' && !Basic::$config->PRODUCTION_MODE)
+		if ($this->contentType == 'text/html' && !Basic::$config->PRODUCTION_MODE)
 		{
-			echo '<hr style="clear:both;"/><fieldset class="log"><legend>Statistics</legend>'. $this->engine->log->output() .'</fieldset>';
-			echo '<fieldset class="log"><legend>Logs</legend><pre>'. implode('<br />', $this->engine->log->logs) .'</pre></fieldset>';
+			echo '<hr style="clear:both;"/><fieldset class="log"><legend>Statistics</legend>'. Basic::$log->getTimers() .'</fieldset>';
+			echo '<fieldset class="log"><legend>Logs</legend><pre>'. Basic::$log->getLogs() .'</pre></fieldset>';
 
-			if (is_object($this->database))
-				echo '<fieldset class="log"><legend>Database</legend>'. $this->database->_explains .'</fieldset>';
+			if (isset(Basic::$database))
+				echo '<fieldset class="log"><legend>Database</legend>'. Basic::$database->_explains .'</fieldset>';
 		}
 	}
 
---
+++ library/Basic/Database.php
@@ -0,0 +1,184 @@
+<?php
+
+class Basic_Database
+{
+	private $_config;
+	private $_queryLog = array();
+	private $_queryCount = array();
+	private $_link;
+	private $_queryResult;
+
+	public $queryTotalRows;
+
+	var $result_row;
+	var $query_counter;
+	var $query_log;
+	var $_explains = '';
+
+	public function __construct()
+	{
+		$this->_config =& Basic::$config->Database;
+	}
+
+	private function _connect()
+	{
+		Basic::$log->start();
+
+		if ($this->_config['persistentConnect'])
+			$this->_link = mysql_pconnect($this->_config['host'], $this->_config['username'], $this->_config['password']);
+		else
+			$this->_link = mysql_connect($this->_config['host'], $this->_config['username'], $this->_config['password']);
+
+		if (false == $this->_link)
+			throw new Basic_Database_CouldNotConnectException('An error occured `%s`', array(mysql_error()));
+
+		if (false == mysql_select_db($this->_config['database']))
+			throw new Basic_Database_CouldNotSelectDatabaseException('An error occured `%s`', array(mysql_error()));
+
+		Basic::$log->end($this->_config['database']);
+
+		return TRUE;
+	}
+
+	public function query($query)
+	{
+		if (!isset($this->_link))
+			$this->_connect();
+
+		Basic::$log->start();
+
+		$queryType = array_shift(preg_split('/\s+/', trim($query)));
+		$this->_queryResult = mysql_query($query);
+
+		if (false == $this->_queryResult)
+			throw new DatabaseException('query_error', array('error'=>mysql_error(), 'query'=>$this->query));
+
+		// Store the number of affected/returned rows
+		if (in_array($queryType, array('SELECT', 'SHOW', 'DESCRIBE', 'EXPLAIN')))
+			$result = mysql_num_rows($this->_queryResult);
+		elseif (mysql_affected_rows() > -1)
+			$result = mysql_affected_rows();
+		else
+			$result = $this->_queryResult;
+
+		if (false !== strpos($this->query, 'SQL_CALC_FOUND_ROWS'))
+		{
+			Basic::$log->write('Populated FOUND_ROWS');
+			$this->queryTotalRows = array_pop(mysql_fetch_assoc(mysql_query("SELECT FOUND_ROWS();")));
+		}
+
+		Basic::$log->end($result .' > '. preg_replace('~[\s|\n|\t| ]+~', ' ', $query));
+
+		if (!Basic::$config->PRODUCTION_MODE && 'SELECT' == $queryType)
+			$this->_explains .= $this->explain($query);
+
+		return $result;
+	}
+
+	public function fetchNext($column = NULL)
+	{
+		if (!is_resource($this->_queryResult))
+			return FALSE;
+
+		$row = mysql_fetch_assoc($this->_queryResult);
+
+		if (isset($column))
+			return $row[ $column ];
+		else
+			return $row;
+	}
+
+	public function fetchAll($column = NULL)
+	{
+		if (!is_resource($this->_queryResult))
+			return FALSE;
+
+		$rows = array();
+
+		while($row = mysql_fetch_assoc($this->_queryResult))
+			if (isset($column))
+				array_push($rows, $row[ $column ]);
+			else
+				array_push($rows, $row);
+
+		$this->_clean();
+
+		return $rows;
+	}
+
+	public function fetchNextObject($className = null)
+	{
+		if (!is_resource($this->_queryResult))
+			return FALSE;
+
+		$class = new $className;
+		$class->_load($this->fetchNext());
+
+		return $class;
+	}
+
+	public function fetchAllObjects($className = null)
+	{
+		if (!is_resource($this->_queryResult))
+			return FALSE;
+
+		$results = array();
+		while ($row = $this->fetchNext())
+		{
+			$class = new $className;
+			$class->_load($row);
+			$results[ $class->id ] = $class;
+		}
+
+		$this->_clean();
+
+		return $results;
+	}
+
+	private function _clean()
+	{
+		if (isset($this->_queryResult) && is_resource($this->_queryResult))
+			mysql_free_result($this->_queryResult);
+
+		unset($this->_queryResult, $this->queryTotalRows);
+	}
+
+	public function explain($query)
+	{
+		return $this->show(mysql_query('EXPLAIN '. $query), $query);
+	}
+
+	public function show($result, $query)
+	{
+		if (isset($result))
+		{
+			$_result = $this->_queryResult;
+			$this->_queryResult = $result;
+		}
+
+		$output = '<table width="100%" border="1" cellspacing="0" cellpadding="5"><caption>'. htmlspecialchars($query) .'</caption><thead><tr>';
+		mysql_data_seek($result, 0);
+		for ($i = 0; $i < mysql_num_fields($result); $i++)
+			$output .= '<th>'. mysql_field_name($result, $i) .'</th>';
+
+		$output .= '</tr></thead><tbody>';
+		while ($row = $this->fetch_next())
+		{
+			$output .= '<tr>';
+			for ($i = 0; $i < mysql_num_fields($result); $i++)
+				$output .= '<td>'. $row[mysql_field_name($result, $i)] .'</td>';
+			$output .= '</tr>';
+		}
+		$output .= '</tbody></table>';
+
+		if (isset($_result))
+			$this->_queryResult = $_result;
+
+		return $output;
+	}
+
+	public static function escape($string)
+	{
+		return mysql_real_escape_string(preg_replace ('/[\x00-\x08\x0B\x0C\x0E-\x1F]/', '', $string));
+	}
+}
