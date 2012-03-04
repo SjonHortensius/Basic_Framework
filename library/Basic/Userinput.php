7aeae3e9e3193d5d7de97eb24847318bbc4d2bda
Sjon Hortensius <sjon@hortensius.net>
2012-03-04 11:59:02 +0100

    Use IteratorAggregate in Userinput, assign things to $this
    Basic_Userinput
    	- replaced custom Iterator by IteratorAggregate
    	- don't store values in custom array, just assign them on $this
    	- all magic methods (except __set) are called only for non-existing
    values; so make them throw Exceptions
    	- __set; removed setting of source.action, it is gone
    Basic_UserinputValue
    	- removed support for source.action, just check if we exist in
    Basic::$config->Userinput
    	- _getRawValue is now public

--- library/Basic/Userinput.php
+++ library/Basic/Userinput.php
@@ -1,15 +1,11 @@
 <?php
 
-class Basic_Userinput implements ArrayAccess, Iterator
+class Basic_Userinput implements ArrayAccess, IteratorAggregate
 {
 	protected $_config = array();
-	// we cannot directly assign values to $this since we would be unable to intercept __isset calls (which requires a lot of code updates)
-	protected $_values = array();
-	protected $_actionValues; // For Iterator
 
 	public function __construct()
 	{
-		// Cast to array
 		foreach (Basic::$config->Userinput as $name => $config)
 		{
 			$config->source = (array)$config->source;
@@ -19,8 +15,6 @@ class Basic_Userinput implements ArrayAccess, Iterator
 
 	public function init()
 	{
-		$this->_values = array();
-
 		foreach ($this->_config as $name => $config)
 			$this->$name = $config;
 	}
@@ -42,15 +36,12 @@ class Basic_Userinput implements ArrayAccess, Iterator
 
 	public function __isset($name)
 	{
-		return isset($this->_values[ $name ]);
+		return false;
 	}
 
 	public function __get($name)
 	{
-		if (!isset($this->_values[ $name ]))
-			throw new Basic_Userinput_UndefinedException('The specified input `%s` is not configured', array($name));
-
-		return $this->_values[ $name ];
+		throw new Basic_Userinput_UndefinedException('The specified input `%s` is not configured', array($name));
 	}
 
 	public function __set($name, array $config)
@@ -58,18 +49,12 @@ class Basic_Userinput implements ArrayAccess, Iterator
 		if ('_' == $name[0])
 			throw new Basic_Userinput_InvalidNameException('`%s` has an invalid name', array($name));
 
-		if (!isset($config['source']['action']))
-			$config['source']['action'] = array(Basic::$controller->action);
-
-		$this->_values[ $name ] = new Basic_UserinputValue($name, $config);
+		$this->$name = new Basic_UserinputValue($name, $config);
 	}
 
 	public function __unset($name)
 	{
-		if (!isset($this->_values[ $name ]))
-			throw new Basic_Userinput_UndefinedException('The specified input `%s` is not configured', array($name));
-
-		unset($this->_values[ $name ]);
+		throw new Basic_Userinput_UndefinedException('The specified input `%s` is not configured', array($name));
 	}
 
 	protected function _getFormData()
@@ -158,19 +143,8 @@ class Basic_Userinput implements ArrayAccess, Iterator
 	public function offsetSet($name, $value){	throw new Basic_NotSupportedException('');	}
 	public function offsetUnset($name){			throw new Basic_NotSupportedException('');	}
 
-    public function rewind()
-    {
-		$this->_actionValues = array();
-
-		foreach ($this->_values as $name => $value)
-			if ($value->isGlobal() || in_array(Basic::$controller->action, $value->source['action']))
-				$this->_actionValues[$name] = $value;
-
-		return reset($this->_actionValues);
-    }
-
-    public function current(){	return current($this->_actionValues);	}
-    public function key(){		return key($this->_actionValues);		}
-    public function next(){		next($this->_actionValues);				}
-    public function valid(){	return false !== $this->current();		}
+	public function getIterator()
+	{
+		return new ArrayIterator($this);
+	}
 }
