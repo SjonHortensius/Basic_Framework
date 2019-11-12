640c704671f25a7338a9b102ee3cd8d129b9a8dd
Sjon Hortensius <sjon@hortensius.net>
2019-11-12 13:26:40 +0100

    Memcache - introduce lockedGet() to prevent cache stampedes

--- library/Basic/Memcache.php
+++ library/Basic/Memcache.php
@@ -59,6 +59,66 @@ class Basic_Memcache extends Memcached
 	}
 
 	/**
+	 * Extends @see Basic_Memcache::get, adds locking to prevent cold-cache-stampedes. This makes sure a cache-miss
+	 * will only be refreshed on one client, while other clients wait until that client is done and the cache is filled
+	 *
+	 * @param string $key Key of item to retrieve
+	 * @param null $cache_cb Callback that gets called when value is not found. Should return the actual value
+	 * @param null $ttl Time-to-live value used when using the value from the callback
+	 * @return mixed
+	 */
+	public function lockedGet($key, $cache_cb = null, $ttl = null)
+	{
+		$tries = 5;
+
+		if (!class_exists('Basic_Memcache_Locked'))
+			eval('class Basic_Memcache_Locked {}');
+
+		try
+		{
+			// don't pass CB because we want our custom logic to handle misses
+			while (--$tries > 0 && ($result = $this->get($key)) instanceof Basic_Memcache_Locked)
+			{
+				error_log(__METHOD__ .' - waiting for unlock of '. $key);
+				sleep(1);
+			}
+
+			if ($tries == 0)
+			{
+				error_log(__METHOD__ .' - timeout, removing lock for '. $key);
+				$this->delete($key);
+
+				$result = $this->get($key, $cache_cb, $ttl);
+			}
+		}
+		catch (Basic_Memcache_ItemNotFoundException $e)
+		{
+			if (!isset($cache_cb))
+				throw $e;
+
+			try
+			{
+				// add() only succeeds when entry isn't present
+				$this->add($key, new Basic_Memcache_Locked, $ttl);
+			}
+			catch (Basic_Memcache_ItemAlreadyExistsException $e)
+			{
+				// key was already added - verify it's not locked
+				// don't pass CB because another thread is apparantly refreshing
+				return $this->lockedGet($key);
+			}
+
+			ignore_user_abort(true);
+
+			// we have obtained the lock, execute CB and overwrite lock with result
+			$result = call_user_func($cache_cb);
+			$this->set($key, $result, $ttl);
+		}
+
+		return $result;
+	}
+
+	/**
 	 * Overload the default @see Memcached::set - adds logging and exceptions
 	 */
 	public function set($key, $value, $expiration = null)
