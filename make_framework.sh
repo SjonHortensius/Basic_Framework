df1d8420891b20e1fccddbf4a955060582b1f7e4
Sjon Hortensius <sjon@hortensius.net>
2020-02-25 17:00:48 +0100

    make_framework - make robuster by implementing some shellcheck hints

--- make_framework.sh
+++ make_framework.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+set -ue
 
-cd `dirname $0`
+cd "$(dirname "$0")"
 
 # use tempfile to prevent renewing mtime
-F=`mktemp`
-echo -e '<?php\n/**\n' >$F
-sed "s/CLIENT/$CLIENT/g" LICENSE | sed "s/APPLICATION/$APP/g" >> $F
-echo -e '\n*/\n' >>$F
+F=$(mktemp)
+echo -e '<?php\n/**\n' >"$F"
+sed "s/CLIENT/$CLIENT/g" LICENSE | sed "s/APPLICATION/$APP/g" >>"$F"
+echo -e '\n*/\n' >>"$F"
 
-find ./library/ -type f -name \*.php | while read f
+while read -r f
 do
-	php -w $f | grep -v '^<?php' >> $F
-done
+	php -w "$f" | grep -v '^<?php' >>"$F"
+done < <(find ./library/ -type f -name \*.php)
 
-echo 'Basic::bootstrap();' >> $F
+echo 'Basic::bootstrap();' >>"$F"
 
 # for rsync; make mtime equal to last actual modification, not now
-touch -d "`du -s --time .|cut -f2`" $F
-mv -f $F framework.inc.php
+touch -d "$(du -s --time .|cut -f2)" "$F"
+mv -f "$F" framework.inc.php
 
 chmod -w,+r framework.inc.php
